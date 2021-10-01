71fc756fa0ce75f3559b98f7bd3465a634abbfdb
Jia Lu <pochialu@gmail.com>
2021-10-01 09:34:22 +0800

    feat: create property binding

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-sap.ui.require(["sap/m/Text"], function (Text) {
-  "use strict";
+sap.ui.require(
+  ["sap/m/Text", "sap/ui/model/json/JSONModel"],
+  function (Text, JSONModel) {
+    "use strict";
 
-  // Attach an anonymous function to the SAPUI5 'init' event
-  sap.ui.getCore().attachInit(function () {
-    // Create a text UI element that displays a hardcoded text string
-    new Text({ text: "Hi, my name is Harry Hawk" }).placeAt("content");
-  });
-});
+    // Attach an anonymous function to the SAPUI5 'init' event
+    sap.ui.getCore().attachInit(function () {
+      // Create a JSON model from an object literal
+      var oModel = new JSONModel({
+        greetingText: "Hi, my name is Harry Hawk",
+      });
+
+      // Assign this local model object to the entire SAPUI5 core, leading to all controls within the app can access it
+      sap.ui.getCore().setModel(oModel);
+
+      // sap.m.Text control's property text whose value set to {...}, binding to greetingText property at the root of the default model
+      new Text({ text: "{/greetingText}" }).placeAt("content");
+    });
+  }
+);
