30218b006656fd0160f193d406f6d09df6944c8c
Jia Lu <pochialu@gmail.com>
2021-10-01 09:52:00 +0800

    feat: create two-way data binding

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 sap.ui.require(
-  ["sap/m/Text", "sap/ui/model/json/JSONModel"],
-  function (Text, JSONModel) {
+  ["sap/ui/model/json/JSONModel", "sap/ui/core/mvc/XMLView"],
+  function (JSONModel, XMLView) {
     "use strict";
 
     // Attach an anonymous function to the SAPUI5 'init' event
     sap.ui.getCore().attachInit(function () {
       // Create a JSON model from an object literal
       var oModel = new JSONModel({
-        greetingText: "Hi, my name is Harry Hawk",
+        firstName: "Harry",
+        lastName: "Hawk",
+        enabled: true,
+        panelHeaderText: "Data Binding Basics",
       });
 
       // Assign this local model object to the entire SAPUI5 core, leading to all controls within the app can access it
       sap.ui.getCore().setModel(oModel);
 
-      // sap.m.Text control's property text whose value set to {...}, binding to greetingText property at the root of the default model
-      new Text({ text: "{/greetingText}" }).placeAt("content");
+      // Display the XML view called "App"
+      new XMLView({
+        viewName: "binding.demo.view.App",
+      }).placeAt("content");
     });
   }
 );
